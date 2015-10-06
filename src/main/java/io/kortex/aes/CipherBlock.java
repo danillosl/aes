79077f28f866f10c0b48fab7c84b7e7e1abf46c9
danillosl <danillosl@gmail.com>
2015-10-05 21:07:15 -0300

    corrigindo problemas apontados pelo sonar

--- src/main/java/io/kortex/aes/CipherBlock.java
+++ src/main/java/io/kortex/aes/CipherBlock.java
@@ -27,11 +27,12 @@ public class CipherBlock {
     }
 
     @Override
-    public int hashCode(){
-    	return 1;
+    public int hashCode() {
+        return 1;
     }
+
     @Override
-    public boolean equals (Object o){
+    public boolean equals(Object o) {
         return true;
     }
 }
